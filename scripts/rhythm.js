a6733a028c91cb2406f96bbb5c41b7b59910110b
Makoto Kawaguchi <cebasypia.tskm8@gmail.com>
2020-01-10 01:43:26 +0900

    JS rhythm.js change rhythm Construction from Array to Object (important!) modify convertCsv() to csvToJSON()

--- scripts/rhythm.js
+++ scripts/rhythm.js
@@ -1,128 +1,69 @@
-const BEATS_OBJECT = {
-    value: 0,
-    name: "",
-    note: 0
-};
-const SYMBOL_OBJECT = {
-    event: "",
-    args: []
-};
-const BAR_OBJECT = {
-    beats: JSON.parse(JSON.stringify(BEATS_OBJECT)),
-    tempo: 0,
-    tick: 0,
-    symbol: JSON.parse(JSON.stringify(SYMBOL_OBJECT)),
-    count: 0
-};
+const BAR = "bar";
+const NOTE = "note";
+const BEATS = "beats";
+const TEMPO = "tempo";
+const JUMP = "jump";
+const TO = "to";
+const TIME = "time";
+const BARS = "bars";
 
-const BEATS_NAMES = [
-    { name: "oneFour", str: "1/4", number: 0.25 },
-    { name: "twoFour", str: "2/4", number: 0.50 },
-    { name: "threeFour", str: "3/4", number: 0.75 },
-    { name: "fourFour", str: "4/4", number: 1 }
-];
+const RHYTHM_FILE_ID = "rhythm--file";
 
-export let rhythm = [JSON.parse(JSON.stringify(BAR_OBJECT))];
 import { setBarsElements } from "./bar.js";
 
+export const rhythm = {};
+
 export function addRhythmFileEvent() {
-    document.getElementById("rhythm--file").addEventListener("change", (e) => {
+    document.getElementById(RHYTHM_FILE_ID).addEventListener("change", (e) => {
         let result = e.target.files[0];
         const reader = new FileReader();
         reader.readAsText(result);
         reader.addEventListener('load', function () {
-            convertCsv(reader.result);
+            csvToJSON(reader.result);
         });
     });
 }
 
-function convertCsv(csvText) {
+function csvToJSON(csvText) {
     csvText = csvText.split(/\r\n|\n/);
     csvText = csvText.map(i => i.split(","));
-    for (let i = 0; i <= csvText[0][csvText[0].length - 1]; i++) {
-        rhythm[i] = JSON.parse(JSON.stringify(BAR_OBJECT));
-    }
-    for (let bar = 0; bar < csvText[0].length; bar++) {
-        for (let prop = 0; prop < csvText.length; prop++) {
-            if (csvText[prop][bar] !== "") {
+    for (let column = 0; column <= csvText[0].length - 1; column++) {
+        const prop = csvText[0][column];
+        for (let row = 1; row < csvText.length; row++) {
+            const value = csvText[row][column];
+            if (value) {
                 switch (prop) {
-                    case 0:
+                    case BAR:
+                        if (!rhythm[value]) {
+                            rhythm[value] = { count: 0 };
+                            rhythm[BARS] = value;
+                        }
                         break;
-                    case 1:
-                        rhythm[csvText[0][bar]].beats.note = beatsNoteFrom(csvText[prop][bar]);
-                        console.log(rhythm[csvText[0][bar]].beats);
+                    case NOTE:
+                        const molecule = convertToNumber(value.split("/")[0]);
+                        const denominator = convertToNumber(value.split("/")[1]);
+                        rhythm[csvText[row][0]][prop] = (molecule / denominator);
                         break;
-                    case 2:
-                        rhythm[csvText[0][bar]].beats.name = beatsNameFrom(csvText[prop][bar]);
-                        rhythm[csvText[0][bar]].beats.value = beatsValueFrom(csvText[prop][bar]);
+                    case BEATS:
+                        rhythm[csvText[row][0]][prop] = value;
                         break;
-                    case 3:
-                        rhythm[csvText[0][bar]].tempo = csvText[prop][bar];
-                        rhythm[csvText[0][bar]].tick = 60 * 1000 / csvText[prop][bar];
+                    case TEMPO:
+                        rhythm[csvText[row][0]][prop] = convertToNumber(value);
                         break;
-                    case 4:
-                        rhythm[csvText[0][bar]].symbol = symbolObjectFrom(csvText[prop][bar]);
+                    case TO:
+                    case TIME:
+                        if (!(JUMP in rhythm[csvText[row][0]])) {
+                            rhythm[csvText[row][0]][JUMP] = {};
+                        }
+                        rhythm[csvText[row][0]][JUMP][prop] = convertToNumber(value);
                         break;
                     default:
                 }
             }
         }
     }
-    for (let i = 1; i < rhythm.length; i++) {
-        if (rhythm[i].beats.value === 0) {
-            rhythm[i].beats.value = rhythm[i - 1].beats.value;
-        }
-        if (rhythm[i].beats.name === "") {
-            rhythm[i].beats.name = rhythm[i - 1].beats.name;
-        }
-        if (rhythm[i].beats.note === 0) {
-            rhythm[i].beats.note = rhythm[i - 1].beats.note;
-        }
-        if (rhythm[i].tempo === 0) {
-            rhythm[i].tempo = rhythm[i - 1].tempo;
-        }
-        if (rhythm[i].tick === 0) {
-            rhythm[i].tick = rhythm[i - 1].tick;
-        }
-    }
     console.log(rhythm);
-    setBarsElements(rhythm.length - 1);
-};
-
-function beatsValueFrom(beatsValueText) {
-    return convertToNumber(beatsValueText.split("/")[0]);
-};
-
-function beatsNameFrom(beatsNameText) {
-    let result;
-    BEATS_NAMES.forEach((obj) => {
-        if (obj.str === beatsNameText) {
-            result = obj.name;
-        }
-    })
-    return result;
-};
-
-function beatsNoteFrom(beatsNoteText) {
-    let molecule = convertToNumber(beatsNoteText.split("/")[0]);
-    let denominator = convertToNumber(beatsNoteText.split("/")[1]);
-    return (molecule / denominator);
-};
-
-function symbolObjectFrom(symbolText) {
-    let symbolObject = JSON.parse(JSON.stringify(SYMBOL_OBJECT));
-    let tempSymbol = symbolText.split("-");
-    tempSymbol = tempSymbol.map(i => convertToNumber(i));
-    for (let i = 0; i < tempSymbol.length; i++) {
-        switch (i) {
-            case 0:
-                symbolObject.event = tempSymbol[i];
-                break;
-            default:
-                symbolObject.args.push(tempSymbol[i]);
-        }
-    }
-    return symbolObject;
+    setBarsElements(rhythm[BARS]);
 };
 
 function convertToNumber(str) {
